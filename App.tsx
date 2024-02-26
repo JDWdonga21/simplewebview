e4149e71e31db73258906e4cf7e3e1f1c9189f58
jungDongWook <jdwdonga21@zeroweb.kr>
2024-02-26 11:03:48 +0900

    상태바 투명하게

--- App.tsx
+++ App.tsx
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import { SafeAreaView, StatusBar } from "react-native";
-import {SafeAreaProvider} from 'react-native-safe-area-context'
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, View, Dimensions } from "react-native";
+import {SafeAreaProvider, useSafeAreaInsets} from 'react-native-safe-area-context'
 import { WebView } from 'react-native-webview';
+
+StatusBar.setBackgroundColor("transparent");
+StatusBar.setTranslucent(true);
+StatusBar.setBarStyle("dark-content");
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 class App extends Component {
   render() {
     return (
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
         <StatusBar/>
-        <WebView 
-          source={{ uri: 'https://webnotice.netlify.app/' }} 
-        />
+        <View style={styles.scrollView}>
+          <>
+            <WebView 
+              source={{ uri: 'https://webnotice.netlify.app/' }} 
+            />
+          </>
+        </View>        
       </SafeAreaView>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  scrollView: {
+    flex: 1,
+    width: windowWidth,
+    height: windowHeight,
+  },
+})
 export default App;
