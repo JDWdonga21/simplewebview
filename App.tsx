972fc46543a5bf5d22c1a6044cf107ca279eb79b
jungDongWook <jdwdonga21@zeroweb.kr>
2024-02-27 18:42:02 +0900

    뒤로가기 버튼 : 종료 알림 실행

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { Component, useRef } from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, View, Dimensions, BackHandler } from "react-native";
+import { SafeAreaView, StatusBar, StyleSheet, View, Dimensions, BackHandler, TouchableOpacity, Text, Alert, Button } from "react-native";
 import {SafeAreaProvider, useSafeAreaInsets} from 'react-native-safe-area-context'
 import { WebView } from 'react-native-webview';
 
@@ -23,16 +23,40 @@ class App extends Component {
 
   handleBackButton = () =>{
     if(this.webView.current && this.webView.current.goBack){
+      console.log("웹 뒤로가기");
+      Alert.alert(
+        "종료하시겠습니까?", 
+        "",
+        [
+          {
+            text: "아니요",
+            onPress: () => {},
+            style: 'cancel'
+          },
+          {
+            text: "예",
+            onPress: this.handleExitButton,
+          }],
+          {cancelable: false}
+      )
       this.webView.current.goBack();
       return true;
     }
     return false;
   }
+
+  handleExitButton = () => {
+    BackHandler.exitApp();
+  } 
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <StatusBar/>
         <View style={styles.scrollView}>
+          {/* 앱 종료 버튼
+          <TouchableOpacity style={styles.exitButton} onPress={this.handleExitButton}>
+            <Text style={styles.exitButtonText} > 앱 종료 </Text>
+          </TouchableOpacity> */}
           <>
             <WebView 
               ref={this.webView}
@@ -53,5 +77,14 @@ const styles = StyleSheet.create({
     width: windowWidth,
     height: windowHeight,
   },
+  exitButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    margin: 10,
+    alignItems: 'center',
+  },
+  exitButtonText: {
+    color: 'white',
+  }
 })
 export default App;
